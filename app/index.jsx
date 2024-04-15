630e5001c0e6625fe07e1241657a19dffacb89f1
Pol Gubau Amores <gubaupol@gmail.com>
2024-04-15 21:00:31 +0200

    Update babel.config.js, App.js, app.json, and package.json

---
+++ app/index.jsx
@@ -0,0 +1,50 @@
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        {/*  */}
+        <View className="items-center justify-center w-full px-4 min-h-[85vh]">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px] "
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] h-[300px] w-full"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover endless possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-white text-sm font-pregular text-center mt-7">
+            Explore the world of digital assets and discover the latest trends.
+          </Text>
+          <CustomButton
+            title="Continue with email"
+            handlePress={() => {
+              router.push("/sign-in");
+            }}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar style="light" backgroundColor="#161622" />
+    </SafeAreaView>
+  );
+}
